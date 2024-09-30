3af27e5b70e97695a9e0066f7214689753ab1ef9
devjeremyjake <devjeremyjake@gmail.com>
2024-09-30 12:49:28 +0100

    Left and Right side bars

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import LeftSideBar from '@/components/shared/LeftSideBar';
 import Navbar from '@/components/shared/navbar/navbar';
+import RightsideBar from '@/components/shared/RightsideBar';
 import { PropsWithChildren } from 'react';
 
 const Layout = ({ children }: PropsWithChildren) => {
@@ -6,13 +8,13 @@ const Layout = ({ children }: PropsWithChildren) => {
 		<main className="background-light850_dark100 relative">
 			<Navbar />
 			<div className="flex">
-				LefrsideBar
+				<LeftSideBar />
 				<section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
 					<div className="mx-auto w-full max-w-5xl">{children}</div>
 				</section>
-				RightsideBar
+				<RightsideBar />
 			</div>
-			Toaster
+			{/* Toaster */}
 		</main>
 	);
 };
